cd5bcb0860f96fd159ca37e33f314ce5b8bcb0f4
A-Badawi <Ahmad.badawi369@gmail.com>
2023-10-15 00:23:29 +0300

    Resolved merge conflict by keeping local changes.

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <AppRouter /> {/* Render the AppRouter component */}
+      <Admin /> {/* Render the AppRouter component */}
     </div>
   );
 }
